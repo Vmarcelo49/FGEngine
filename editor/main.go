9f7f823a1d511b77ee2b0f22dfc61e99473fedae
Vmarcelo49 <vmarcelo49@gmail.com>
2025-08-13 17:56:26 -0300

    Add Game struct and layout method; update Go version and dependencies

--- editor/main.go
+++ editor/main.go
@@ -1,1 +1,37 @@
 package editor
+
+import (
+	"github.com/ebitengine/debugui"
+)
+
+const (
+	WindowWidth  = 1600
+	WindowHeight = 900
+)
+
+type Game struct {
+	debugui debugui.DebugUI
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
+}
+
+/*
+func main() {
+	ebiten.SetWindowTitle("Animation Editor")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeDisabled)
+	ebiten.SetWindowSize(WindowWidth, WindowHeight)
+
+	game := &Game{
+		imageCache:        newImageCache(),
+		zoom:              DefaultZoom,
+		zoomSelectorIndex: 1,
+		anim:              AnimationState{},
+	}
+
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
+}
+*/
--- go.mod
+++ go.mod
@@ -1,14 +1,14 @@
 module fgengine
 
-go 1.24.5
+go 1.25.0
 
 require (
+	github.com/ebitengine/debugui v0.2.0-alpha.6.0.20250813070520-e1125f6351c9
 	github.com/hajimehoshi/ebiten/v2 v2.9.0-alpha.13.0.20250813034658-e67f7e3ae298
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
-	github.com/ebitengine/debugui v0.2.0-alpha.6.0.20250813070520-e1125f6351c9 // indirect
 	github.com/ebitengine/gomobile v0.0.0-20250329061421-6d0a8e981e4c // indirect
 	github.com/ebitengine/hideconsole v1.0.0 // indirect
 	github.com/ebitengine/purego v0.9.0-alpha.10 // indirect
